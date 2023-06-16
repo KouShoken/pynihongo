041d5c84899ae11d7ae8228f256665f764be793f
KouShoken <koshokenjcx@gmail.com>
2023-06-17 00:34:39 +0900

    清音に対するプログラム　完成

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""
+@Project: https://github.com/KouShoken/pynihongo
+@Time: 西暦2023年6月16日 15:30
+@Author: KouShoken
+
+This is an open source project that uses the MIT protocol. 
+While I don't make any demands, But　I think respecting copyright 
+maybe is a basic morality.
+"""
+
+from setuptools import setup
+
+setup(
+    name='PyNihongo',
+    version='0.0.1',
+    author='Koushoken',
+    author_email='kskjcx-dev@yahoo.co.jp',
+    description='A Python package about japanese.',
+    packages=['pynihongo'],
+    install_requires=[
+
+    ],
+)
